f81437a7bc30a56b828762c53b21e622e735fc10
NikolaiProgramist <nikluk2005@mail.ru>
2024-02-10 22:12:06 +0300

    Hello codewars!

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+// Multiply
+
+//function multiply(int $a, int $b): int {
+//    return $a * $b;
+//}
+
+// Remove exclamation marks
+
+//function remove_exclamation_marks(string $string): string {
+//    return str_replace('!', '', $string);
+//}
+
+// Sum Arrays
+
+//function sum(array $a): float
+//{
+//    $sum = 0;
+//
+//    if (!empty($a)) {
+//        for ($i = 0; $i < count($a); $i++) {
+//            $sum += $a[$i];
+//        }
+//    }
+//
+//    return $sum;
+//}
+
+//function square_sum($numbers) : int
+//{
+//    $square = 0;
+//
+//    foreach ($numbers as $number) {
+//        $square += $number ** 2;
+//    }
+//
+//    return $square;
+//}
+//
+//echo square_sum([]);
+
+// Function 1 - hello world
+
+//function greet(): string
+//{
+//    $arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//
+//    return $arr[7] . $arr[4] . $arr[11] . $arr[11] . $arr[14] . ' ' . $arr[22] . $arr[14] . $arr[17] . $arr[11] . $arr[3] . '!';
+//}
